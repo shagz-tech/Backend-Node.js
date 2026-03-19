a9754defcfe4be66ded9b5035c2c0b210925351d
shagun <shagung072@gmail.com>
2026-03-19 08:35:28 +0530

    commit

---
+++ index3.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const user = [
+    { id: 1, name:"shagun", branch:"CSE", city: "Pune"},
+    { id: 2, name:"sona", branch:"IT", city: "Mumbai"},
+    { id: 3, name:"shagu", branch:"CS", city: "Delhi"},
+];
+
+app.get("/users",(req,res)=>{
+    const html=`
+    <ul>
+      ${users.map(user=>`<li>${user.name} has branch ${user.branch}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+});
+
+app.get("/names",(req,res)=>{
+    const html=`
+    <ul>
+      ${users.map(user=>`<li>my name is ${user.name} and I am from ${user.city} and I am in {user.branch} branch</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+})
+
+app.get("/api/users",(req,res) =>{
+    return res.json(users);
+});
+
+app.get("/api/users/:id",(req,res)=>{
+    const id = req.params.id;
+    const user=users.find(user=>user.id==id);
+    if(!user){
+        return res.status(404).json({message:"User not found"});
+    }
+    else{
+        return res.json(user);
+    }
+})
+
+app.listen(3003);
