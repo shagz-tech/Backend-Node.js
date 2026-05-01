3a89a05a6d2f93614ca0117fe26d997bc9909bbd
shagun <shagung072@gmail.com>
2026-05-01 20:42:24 +0530

    refactor: improve code formatting and structure for cookie handling

--- index13.js
+++ index13.js
@@ -1,13 +1,33 @@
-const express=require("express");
-const cookieParser=require("cookie-parser");
-const app=express();
-app.use(cookieParser());
-app.get("/selcookie",(req,res)=>{
-    res.cookie("name","shagun");
-    res.send("cookie set successfully");
-});
-app.get("/getcookie",(req,res)=>{
-    console.log("server running on port 3001");
-});
-app.listen(3001);
+const express = require("express");
+const cookieParser = require("cookie-parser");
 
+const app = express();
+
+// middleware
+app.use(cookieParser());
+
+// set cookie
+app.get("/setcookie", (req, res) => {
+    res.cookie("name", "shagun", {
+        maxAge: 1000 * 60 * 2, // 2 minutes
+    });
+    res.send("Cookie set successfully");
+});
+
+// get cookie
+app.get("/getcookie", (req, res) => {
+    const cookies = req.cookies;
+    console.log(cookies);
+    res.send(cookies);
+});
+
+// delete cookie
+app.get("/deletecookie", (req, res) => {
+    res.clearCookie("name");
+    res.send("Cookie deleted successfully");
+});
+
+// server start
+app.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
